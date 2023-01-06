d6b6968e32a16fca0240f26a36f4153712bea1df
Chukwuebuka Akwiwu-Uzoma <41445245+Ebuk-a@users.noreply.github.com>
2023-01-06 07:43:00 +0000

    update localhost to vm ip

--- frontend/src/utils/url/url.js
+++ frontend/src/utils/url/url.js
@@ -1,6 +1,10 @@
-const baseUrl = 'http://34.72.72.238:4000/api/v1'
 const baseImageUrl = 'https://storage.googleapis.com/devops_movie_bucket/image_uploads/'
 const baseMovieUrl = 'https://storage.googleapis.com/devops_movie_bucket/movie_uploads/'
+
+
+const url = location.href.split('//')[1]
+const urlIP = url.split('/')[0]
+let baseUrl = `http://${urlIP}:4000/api/v1`
 
 const api = async (url, data = {}, method = '', token = '') => {
   const options = {
@@ -16,6 +20,7 @@ const api = async (url, data = {}, method = '', token = '') => {
     delete options.body
   }
 
+  console.log(baseUrl)
   try {
     const response = await fetch(`${baseUrl}/${url}`, options)
 
@@ -28,6 +33,7 @@ const api = async (url, data = {}, method = '', token = '') => {
     return responseData
 
   } catch (error) {
+    localStorage.removeItem('$baseUrl')
     throw error
   }
 }
